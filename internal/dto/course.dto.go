a9c25ab246cf32ba93a4a503265f54575b4dc617
Abhishek lohia <abhisheklohia@tripxl.com>
2026-06-24 13:08:20 +0530

    fix dto course

--- internal/dto/course.dto.go
+++ internal/dto/course.dto.go
@@ -1,15 +1,16 @@
 package dto
 
 type CreateCourseRequest struct {
-	CourseTitle          string `json:"course_title" binding:"required"`
-	CourseDescription    string `json:"course_description"`
-	CourseType           string `json:"course_type" binding:"required"`
-	TotalDurationMinutes uint   `json:"total_duration_minutes" binding:"required"`
+	CourseTitle          string `form:"course_title" json:"course_title" binding:"required"`
+	CourseDescription    string `form:"course_description" json:"course_description"`
+	CourseType           string `form:"course_type" json:"course_type" binding:"required"`
+	TotalDurationMinutes uint   `form:"total_duration_minutes" json:"total_duration_minutes" binding:"required"`
 }
+
 type UpdateCourseRequest struct {
-	CourseTitle          string `json:"course_title"`
-	CourseDescription    string `json:"course_description"`
-	CourseType           string `json:"course_type"`
-	IsActive             *bool  `json:"is_active"`
-	TotalDurationMinutes *uint  `json:"total_duration_minutes"`
+	CourseTitle          string `form:"course_title" json:"course_title"`
+	CourseDescription    string `form:"course_description" json:"course_description"`
+	CourseType           string `form:"course_type" json:"course_type"`
+	IsActive             *bool  `form:"is_active" json:"is_active"`
+	TotalDurationMinutes *uint  `form:"total_duration_minutes" json:"total_duration_minutes"`
 }
