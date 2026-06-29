8d4e8de3400edfb04460363b3fe10b1d1e69e125
Abhishek lohia <abhisheklohia@tripxl.com>
2026-06-29 10:45:53 +0530

    add video progress and dark/light mode

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	// automigration "example.com/m/internal/Automigration"
+	automigration "example.com/m/internal/Automigration"
 	"example.com/m/internal/common/publisher"
 	redisclient "example.com/m/internal/common/redis"
 	"example.com/m/internal/common/sse"
@@ -101,9 +102,9 @@ func main() {
 	// fmt.Printf("config: %+v\n", cfg)
 	database := db.SetupDB(cfg)
 
-	// if err := automigration.RunAutoMigration(database, cfg.APP_ENV); err != nil {
-	// 	log.Fatal("migration failed:", err)
-	// }
+	if err := automigration.RunAutoMigration(database, cfg.APP_ENV); err != nil {
+		log.Fatal("migration failed:", err)
+	}
 
 	seeders.SeedRoles(database)
 	seeders.SeedTestUsers(database)
